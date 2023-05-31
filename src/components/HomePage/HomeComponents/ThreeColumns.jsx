0fd28559c1c56c383d99a6b43c57af8b457b0db0
Jakub Marciniak <kubamar02@gmail.com>
2023-05-31 13:43:32 +0200

    add structure and styling to Three Columns

---
+++ src/components/HomePage/HomeComponents/ThreeColumns.jsx
@@ -0,0 +1,31 @@
+function ThreeColumns() {
+    return (
+        <>
+            <div className="tc">
+                    <div className="tc-single">
+                        <h2 className="tc-number">10</h2>
+                        <h5 className="tc-title">oddanych worków</h5>
+                        <p className="tc-text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
+                            elit
+                            viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                    <div className="tc-single">
+                        <h2 className="tc-number">5</h2>
+                        <h5 className="tc-title">wspartych organizacji</h5>
+                        <p className="tc-text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
+                            elit
+                            viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                    <div className="tc-single">
+                        <h2 className="tc-number">7</h2>
+                        <h5 className="tc-title">zorganizowanych zbiórek</h5>
+                        <p className="tc-text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
+                            elit
+                            viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+            </div>
+        </>
+    );
+}
+
+export default ThreeColumns;
